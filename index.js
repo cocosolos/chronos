c6c1582901b64cff5a7472b811677ecb175c85ad
Corey <coreysotiropoulos@gmail.com>
2018-03-14 22:48:04 -0400

    edit task page skeleton

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var tasks = require('./routes/tasks');
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/tasks', (req, res) => res.render('pages/tasks'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
